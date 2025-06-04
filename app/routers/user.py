2a1177762d3fffa625b93ff8d35dd05924a04491
Nikolay Pushkin <adwedelf@gmail.com>
2025-06-05 01:14:03 +0700

    test pre-commit

--- app/routers/user.py
+++ app/routers/user.py
@@ -1,11 +1,9 @@
 from fastapi import APIRouter
 
 router = APIRouter(prefix="/user", tags=["user"])
-
 @router.post("/register")
 async def register():
     pass
-
 @router.post("/login")
 async def login():
     pass
